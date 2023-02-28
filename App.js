5bd3ccea5deaa02bc06f4235476ba2947b27434c
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-02-28 15:46:22 +0300

    recreating the app

--- App.js
+++ App.js
@@ -1,45 +1,20 @@
-import React from "react";
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { ThemeProvider } from "styled-components/";
-import { theme } from "./src/infrastructure/theme/index";
-import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
-import { LocationContextProvider } from "./src/services/location/location.context";
-import { FavoriteContextProvider } from "./src/services/favorites/favorites.context";
-import {
-  useFonts as useOswald,
-  Oswald_400Regular,
-} from "@expo-google-fonts/oswald";
-import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
-import Navigation from "./src/infrastructure/navigation";
-import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [oswaldLoaded] = useOswald({
-    Oswald_400Regular,
-  });
-
-  const [latoLoaded] = useLato({
-    Lato_400Regular,
-  });
-
-  if (!oswaldLoaded || !latoLoaded) {
-    return null;
-  }
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <AuthenticationContextProvider>
-          <FavoriteContextProvider>
-            <LocationContextProvider>
-              <RestaurantsContextProvider>
-                <Navigation />
-              </RestaurantsContextProvider>
-            </LocationContextProvider>
-          </FavoriteContextProvider>
-        </AuthenticationContextProvider>
-      </ThemeProvider>
-
-      <ExpoStatusBar style="auto" />
-    </>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- src/features/restaurant/components/restaurant-list.styles.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components/native";
-import { FlatList } from "react-native";
-
-export const RestaurantList = styled(FlatList).attrs({
-  contentContainerStyle: {
-    padding: 16,
-  },
-})``;
